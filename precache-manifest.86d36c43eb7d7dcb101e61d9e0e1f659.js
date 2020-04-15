e33a6aba72fff0812db78029b11647dea523a4cb
leduc1901 <leduc1901@github.com>
2020-04-15 10:20:57 +0700

    Updates

---
+++ precache-manifest.86d36c43eb7d7dcb101e61d9e0e1f659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ce1bb6b02edb5c4ff4896df580dd32",
+    "url": "/my-cv-remastered/index.html"
+  },
+  {
+    "revision": "dc6475957ea2c47e4703",
+    "url": "/my-cv-remastered/static/css/main.4e3f6e65.chunk.css"
+  },
+  {
+    "revision": "f8245295fd486d1a0841",
+    "url": "/my-cv-remastered/static/js/2.53d691ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv-remastered/static/js/2.53d691ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6475957ea2c47e4703",
+    "url": "/my-cv-remastered/static/js/main.16b8a6b7.chunk.js"
+  },
+  {
+    "revision": "a2a660b758b27e2d227d",
+    "url": "/my-cv-remastered/static/js/runtime-main.aee6621a.js"
+  },
+  {
+    "revision": "1fe027c4db5ced2bd0ef224535515f2a",
+    "url": "/my-cv-remastered/static/media/37046583_1469068633238535_8959388683824267264_o.1fe027c4.jpg"
+  }
+]);
